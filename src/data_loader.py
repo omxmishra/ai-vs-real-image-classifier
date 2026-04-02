0af2e668bfe548fd05422df902c72feec7f454f1
omxmishra <om.mishra.2410@gmail.com>
2026-04-02 20:50:57 +0530

    src data_loader

---
+++ src/data_loader.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+def get_dataloaders(train_dir, test_dir, batch_size=32):
+    
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
+    
+    train_dataset = datasets.ImageFolder(root=train_dir, transform=transform)
+    test_dataset = datasets.ImageFolder(root=test_dir, transform=transform)
+    
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    
